bc29c7678de7155049262fd562729d4c84ffaf73
li <erdai1995@outlook.com>
2018-07-22 17:58:19 +0800

    修改实体代码

--- AIMovieDatabase/crawler/order.py
+++ AIMovieDatabase/crawler/order.py
@@ -9,13 +9,31 @@ order = [
         'user_id': '1',
         'cinema_name': '保利国际影城(宝龙广场店)',
         'movie_name': '我不是药神',
-        'watching_time': '2017.07.14 15:00'
+        'watching_time': '201807170500'
     },
     {
         'user_id': '1',
         'cinema_name': '保利国际影城(宝龙广场店)',
         'movie_name': '邪不压正',
-        'watching_time': '2017.07.18 23:45'
+        'watching_time': '201807180625'
+    },
+    {
+        'user_id': '1',
+        'cinema_name': '保利国际影城(宝龙广场店)',
+        'movie_name': '动物世界',
+        'watching_time': '201807222325'
+    },
+    {
+        'user_id': '1',
+        'cinema_name': '保利国际影城(宝龙广场店)',
+        'movie_name': '摩天营救',
+        'watching_time': '201807240820'
+    },
+    {
+        'user_id': '1',
+        'cinema_name': '保利国际影城(宝龙广场店)',
+        'movie_name': '邪不压正',
+        'watching_time': '201807251630'
     }
 ]
 
--- AIMovieNer/algorithm/timeRange/time_range_interface.py
+++ AIMovieNer/algorithm/timeRange/time_range_interface.py
@@ -2,11 +2,10 @@
     抽取时间范围的接口文件
 """
 import re
-from datetime import datetime
-
+from datetime import datetime, timedelta
 from algorithm.utils.utils import pretreatment
-
-local_time = datetime.now()
+import calendar
+now = datetime.now()
 
 
 def extract_year(input_sentence):
@@ -15,15 +14,16 @@ def extract_year(input_sentence):
     :param input_sentence:
     :return:
     """
-    year = re.search('\d{2}年', input_sentence).group(0)
+    year = re.search('\d{2}年', input_sentence)
     if year:
-        return '20' + year[0:2]
-    if input_sentence.find('前年'):
-        return local_time.year - 2
-    if input_sentence.find('去年'):
-        return local_time.year - 1
-    if input_sentence.find('今年'):
-        return local_time.year
+        num = year.group(0)[0:2]
+        return '20' + num
+    if re.search('前年', input_sentence):
+        return str(now.year - 2)
+    if re.search('去年', input_sentence):
+        return str(now.year - 1)
+    if re.search('今年', input_sentence):
+        return str(now.year)
     return ''
 
 
@@ -35,15 +35,20 @@ def extract_month(input_sentence):
     """
     month = re.search('\d{1,2}月', input_sentence)
     if month:
-        return month[:-1]
-    if input_sentence.find('上上个月'):
-        return local_time.month - 2
-    if input_sentence.find('上个月') or input_sentence.find('上月'):
-        return local_time.month - 1
-    if input_sentence.find('这个月') or input_sentence.find('本月'):
-        return local_time.month
-    if input_sentence.find('下个月') or input_sentence.find('下月'):
-        return local_time.month + 1
+        num = month.group(0)[:-1]
+        return '0' + num if int(num) < 10 else num
+    if re.search('上上个月', input_sentence):
+        num = str(now.month - 2)
+        return '0' + num if int(num) < 10 else num
+    if re.search('上个月|上月', input_sentence):
+        num = str(now.month - 1)
+        return '0' + num if int(num) < 10 else num
+    if re.search('这个月|这月', input_sentence):
+        num = str(now.month)
+        return '0' + num if int(num) < 10 else num
+    if re.search('下个月|下月', input_sentence):
+        num = str(now.month + 1)
+        return '0' + num if int(num) < 10 else num
     return ''
 
 
@@ -55,34 +60,90 @@ def extract_day(input_sentence):
     """
     day = re.search('\d{1,2}(日|号)', input_sentence)
     if day:
-        return day[:-1]
-    if input_sentence.find('大前'):
-        return local_time.day - 3
-    if input_sentence.find('前'):
-        return local_time.day - 2
-    if input_sentence.find('昨'):
-        return local_time.day - 1
-    if input_sentence.find('今'):
-        return local_time.day
-    if input_sentence.find('明'):
-        return local_time.day + 1
-    if input_sentence.find('后天'):
-        return local_time.day + 2
+        num = day.group(0)[:-1]
+        return '0' + num if int(num) < 10 else num
+    if re.search('大前天', input_sentence):
+        num = str(now.day - 3)
+        return '0' + num if int(num) < 10 else num
+    if re.search('前天', input_sentence):
+        num = str(now.day - 2)
+        return '0' + num if int(num) < 10 else num
+    if re.search('昨天', input_sentence):
+        num = str(now.day - 1)
+        return '0' + num if int(num) < 10 else num
+    if re.search('今天', input_sentence):
+        num = str(now.day)
+        return '0' + num if int(num) < 10 else num
+    if re.search('明天', input_sentence):
+        num = str(now.day + 1)
+        return '0' + num if int(num) < 10 else num
+    if re.search('后天', input_sentence):
+        num = str(now.day + 2)
+        return '0' + num if int(num) < 10 else num
+    return ''
+
+
+def extract_week(input_sentence):
+    """
+    抽取周
+    :param input_sentence:
+    :return:
+    """
+    if re.search('上周', input_sentence):
+        start = str(now - timedelta(days=now.weekday()+7))
+        end = str(now - timedelta(days=now.weekday()+1))
+        return start[0:4] + start[5:7] + start[8:10] + '0000' + '-' + end[0:4] + end[5:7] + end[8:10] + '2359'
+    if re.search('本周|这周', input_sentence):
+        # 本周第一天和最后一天
+        start = str(now - timedelta(days=now.weekday()))
+        end = str(now + timedelta(days=6 - now.weekday()))
+        return start[0:4] + start[5:7] + start[8:10] + '0000' + '-' + end[0:4] + end[5:7] + end[8:10] + '2359'
+    if re.search('下周', input_sentence):
+        start = str(now - timedelta(days=now.weekday() - 7))
+        end = str(now + timedelta(days=now.weekday() + 1))
+        return start[0:4] + start[5:7] + start[8:10] + '0000' + '-' + end[0:4] + end[5:7] + end[8:10] + '2359'
+    return ''
 
 
 def extract_time_range(input_sentence):
     input_sentence = pretreatment(input_sentence)
-    local_year = local_time.year
-    local_month = local_time.month
-    local_weekday = local_time.weekday()
-    local_day = local_time.day
-    local_hour = local_time.hour
-    local_minute = local_time.minute
+    if extract_week(input_sentence):
+        return extract_week(input_sentence)
 
-    if re.search('年|月|日|天|时|分|周', input_sentence):
-        return '201807140000-201807302359'
+    if re.search('未观看|没看|未看', input_sentence):
+        time_now = str(now)
+        return time_now[0:4] + time_now[5:7] + time_now[8:10] + time_now[11:13] + time_now[14:16] + '-201809010000'
+
+    year = extract_year(input_sentence)
+    month = extract_month(input_sentence)
+    day = extract_day(input_sentence)
+    if year:
+        if month:
+            if day:
+                return year + month + day + '0000' + '-' + year + month + day + '2359'
+            else:
+                month_range = calendar.monthrange(int(year), int(month))
+                return year + month + "010000" + '-' + year + month + str(month_range[1]) + '2359'
+        else:
+            return year + "01010000" + '-' + year + '12312359'
+    else:
+        if month:
+            if day:
+                return str(now.year) + month + day + '0000' + '-' + str(now.year) + month + day + '2359'
+            else:
+                month_range = calendar.monthrange(now.year, int(month))
+                return str(now.year) + month + '010000' + '-' + str(now.year) + month + str(month_range[1]) + '2359'
+        else:
+            if day:
+                month = '0' + str(now.month) if now.month < 10 else now.month
+                return str(now.year) + month + day + '0000' + '-' + str(now.year) + month + day + '2359'
+
+    if re.search('之前|以前|已看|已观看', input_sentence):
+        time_now = str(now)
+        return '201801010000-' + time_now[0:4] + time_now[5:7] + time_now[8:10] + time_now[11:13] + time_now[14:16]
+
     return ''
 
 
 if __name__ == '__main__':
-    print(extract_time_range('我要查询昨天'))
+    print(extract_time_range('以前'))
